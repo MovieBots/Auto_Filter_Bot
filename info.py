aceced63a1abfbebd6116f66fe7ba6ce63d3f1c6
MovieBots <154045205+MovieBots@users.noreply.github.com>
2026-03-15 19:18:24 +0530

    Update info.py

--- info.py
+++ info.py
@@ -212,9 +212,9 @@ if 'DYNO' in environ:
     APP_NAME = environ.get('APP_NAME')
 else:
     ON_HEROKU = False
-BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
+BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', 'https://flaky-aili-filter-bot-456880b4.koyeb.app/'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://{}/".format(FQDN) if ON_HEROKU or NO_PORT else "https://{}/".format(FQDN, PORT)
+URL = "https://flaky-aili-filter-bot-456880b4.koyeb.app/".format(FQDN) if ON_HEROKU or NO_PORT else "https://flaky-aili-filter-bot-456880b4.koyeb.app/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'dreamXBotz'))
@@ -228,9 +228,9 @@ else:
     ON_HEROKU = False
 HAS_SSL = bool(getenv('HAS_SSL', True))
 if HAS_SSL:
-    URL = "https://{}/".format(FQDN)
+    URL = "https://flaky-aili-filter-bot-456880b4.koyeb.app/".format(FQDN)
 else:
-    URL = "http://{}/".format(FQDN)
+    URL = "https://flaky-aili-filter-bot-456880b4.koyeb.app/".format(FQDN)
 
 # ============================
 # Reactions Configuration
