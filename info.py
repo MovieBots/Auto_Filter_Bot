0e7db97abce512be555c2e951e9bceff96730e82
MovieBots <154045205+MovieBots@users.noreply.github.com>
2026-03-15 20:25:06 +0530

    Update info.py

--- info.py
+++ info.py
@@ -122,7 +122,7 @@ THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "86400"))
 # ============================
 # Channel & Group Links Configuration
 # ============================
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+XOO-8rE-57Q3ZjU1') # Group link for the bot
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+lwmT_ESNTLlkMTJl') # Group link for the bot
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/Arpitbotmovies') # Owner link for the bot
 UPDATE_CHNL_LNK = environ.get('UPDATE_CHNL_LNK', 'https://t.me/+m2-CnUFU_5c1MDQ1') # Update channel link for the bot
 
