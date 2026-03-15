02a961c29d3a99b71286777e5f9630ee016f38c4
MovieBots <154045205+MovieBots@users.noreply.github.com>
2026-03-15 19:09:33 +0530

    Update info.py

--- info.py
+++ info.py
@@ -56,7 +56,7 @@ SUPPORT_CHAT = environ.get('SUPPORT_CHAT', '')  # Support group link (make sure 
 
 # FORCE_SUB 
 auth_req_channels = environ.get("AUTH_REQ_CHANNELS", "-1003844837616")# requst to join Channel for force sub (make sure bot is admin) only for bot ADMINS  
-auth_channels     = environ.get("AUTH_CHANNELS", "100384483761")# Channels for force sub (make sure bot is admin)
+auth_channels     = environ.get("AUTH_CHANNELS", "-100384483761")# Channels for force sub (make sure bot is admin)
 
 # ============================
 # Payment Configuration
